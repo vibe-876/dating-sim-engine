0a50ab82ae6a9024fe1142d5d0062397ab13e923
cam <ayresc17@holyportcollege.org.uk>
2024-05-02 14:25:44 +0100

    Created main common lisp file.

---
+++ src/pasrer.lisp
@@ -0,0 +1,6 @@
+;; Okay, so the plan is to:
+;; parse the file into a usable format,
+;; load all of the file data into some data struct,
+;; and run the script.
+;;
+;; It can't be *that* hard to do...
